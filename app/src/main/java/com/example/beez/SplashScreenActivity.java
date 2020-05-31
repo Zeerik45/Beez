c2ca19fd790d3a597f1ef0580e466dbc15575e9a
Zeerik45 <60458382+Zeerik45@users.noreply.github.com>
2020-05-31 20:56:18 +0500

    added splash screen and app icon

---
+++ app/src/main/java/com/example/beez/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.example.beez;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EasySplashScreen config=new EasySplashScreen(SplashScreenActivity.this)
+                .withFullScreen()
+                .withTargetActivity(StartActivity.class)
+                .withSplashTimeOut(3000)
+                .withBackgroundColor(Color.parseColor("#FFFFFF"))
+                .withLogo(R.drawable.bee);
+
+        View easyscreen=config.create();
+        setContentView(easyscreen);
+
+    }
+}
